b4458a7f93b7100eb4aae1b91d184a29168fc9d3
Hasanat902 <abulhasanat902@gmail.com>
2022-03-25 22:16:30 +0600

    cart calculation added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Product = (props) => {
-    const {title, image, price} = props.product;
+    const {product, handleAddToCart} = props;
+    const {title, image, price} = product;
     return (
         <div className='col-md-4'>
             <div className='card p-2 m-3 border'>
                 <img className='w-50 m-auto img-fluid' src={image} alt="" />
                 <h4>{title.slice(0,10)}</h4>
                 <h5>Price: ${price}</h5>
-                <button className='btn btn-success'>Add to Cart</button>
+                <button onClick={() => handleAddToCart(product)} className='btn btn-success'>Add to Cart</button>
             </div>
         </div>
     );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,7 +3,8 @@ import Product from '../Product/Product';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
-    console.log(products);
+
+    const [cart, setCart] = useState([]);
 
     useEffect( () => {
         fetch('https://fakestoreapi.com/products')
@@ -11,17 +12,23 @@ const Products = () => {
             .then(data => setProducts(data))
     }, [])
 
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='row'>
             <div className="col-md-10 product-details">
                 <div className='row container'>
                     {
-                        products.map(product => <Product key={product.id} product={product}></Product>)
+                        products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
                     }   
                 </div>
             </div>
             <div className="col-md-2 product-cart">
                 <h3>Order Summary</h3>
+                <p>Selected items: {cart.length}</p>
             </div>
         </div>
     );
